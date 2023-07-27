9efdf49cc09cab08c60c5680c0a15204c592fb1c
Fábio Ribeiro <fcmribeiro22@gmail.com>
2023-07-27 22:43:09 +0100

    Add Prettier to VsCode

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 function addTask() {
-    const taskInput = document.getElementById('task-input');
-    const taskList = document.getElementById('task-list');
-  
-    if (taskInput.value.trim() !== '') {
-      const newTask = document.createElement('li');
-      newTask.textContent = taskInput.value;
-      taskList.appendChild(newTask);
-      taskInput.value = '';
-    }
+  const taskInput = document.getElementById("task-input");
+  const taskList = document.getElementById("task-list");
+
+  if (taskInput.value.trim() !== "") {
+    const newTask = document.createElement("li");
+    newTask.textContent = taskInput.value;
+    taskList.appendChild(newTask);
+    taskInput.value = "";
   }
-  
-  // Event listener to add a task when the "Add Task" button is clicked
-  document.getElementById('add-task-btn').addEventListener('click', addTask);
-  
-  // Event listener to add a task when the "Enter" key is pressed in the input field
-  document.getElementById('task-input').addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-      addTask();
-    }
-  });
+}
+
+// Event listener to add a task when the "Add Task" button is clicked
+document.getElementById("add-task-btn").addEventListener("click", addTask);
+
+// Event listener to add a task when the "Enter" key is pressed in the input field
+document.getElementById("task-input").addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
+
+alert("hehe");
