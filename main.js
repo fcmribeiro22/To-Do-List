aa1e0ef3572cd31100063ce13a76da2bbf676649
Fábio Ribeiro <fcmribeiro22@gmail.com>
2023-07-27 22:14:35 +0100

    Create project funations

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{function e(){const e=document.getElementById("task-input"),t=document.getElementById("task-list");if(""!==e.value.trim()){const n=document.createElement("li");n.textContent=e.value,t.appendChild(n),e.value=""}}document.getElementById("add-task-btn").addEventListener("click",e),document.getElementById("task-input").addEventListener("keydown",(t=>{"Enter"===t.key&&e()}))})();
