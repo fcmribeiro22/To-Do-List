32491b02b49b4930c690eedbe3d6f548cd29741e
fabio <fcmribeiro22@gmail.com>
2023-09-02 23:26:26 +0100

    Add UX for tasks

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import "./styles.css";
 import { initiateApp } from "./dom.js";
 
 initiateApp();
+
+console.log("hello");
+
+import { format, addDays } from "date-fns";
+
+const today = new Date();
+const tomorrow = addDays(today, 1);
+console.log(`Today is ${format(today, "yyyy-MM-dd")}`);
+console.log(`Tomorrow will be ${format(tomorrow, "yyyy-MM-dd")}`);
