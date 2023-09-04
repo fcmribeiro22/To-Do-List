f652ca18005d5cd344cd002bf45fc281c4c915a6
fabio <fcmribeiro22@gmail.com>
2023-09-04 22:52:24 +0100

    Add date creation dynamically

--- src/factory.js
+++ src/factory.js
@@ -1,11 +1,16 @@
+import { format, addDays } from "date-fns";
+
 export function createProject(project) {
   return { project, tasks: [], selected: false };
 }
 
 export function createTask(task, description, duedate) {
+  const today = format(new Date(), "yyyy-MM-dd");
+
   return {
     task,
     description,
     duedate,
+    creation: today,
   };
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,3 @@ import { initiateApp } from "./dom.js";
 initiateApp();
 
 console.log("hello");
-
-import { format, addDays } from "date-fns";
-
-const today = new Date();
-const tomorrow = addDays(today, 1);
-console.log(`Today is ${format(today, "yyyy-MM-dd")}`);
-console.log(`Tomorrow will be ${format(tomorrow, "yyyy-MM-dd")}`);
